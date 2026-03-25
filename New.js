ed772783495be119c5aa30493539092e832e5681
Sagar Singh Gour <sagarsinghgour77@gmail.com>
2026-03-25 23:59:58 +0530

    Fix syntax error in Create function

--- New.js
+++ New.js
@@ -9,7 +9,7 @@ function Change(){
 // Write code to get 1st H1 element from a webpage using DOM
 
 function Create() {
-  let element = document.createElement("h1");
+  let element = document.createElement("h1");;;;;
   element.innerText = "Hello Create";
   document.body.appendChild(element);
 }
