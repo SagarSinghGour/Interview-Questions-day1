8ff11834bf45915d8a1dd6de7e1ea45d56e1376f
Sagar Singh Gour <sagarsinghgour77@gmail.com>
2026-04-15 23:41:01 +0530

    Update New.js

--- New.js
+++ New.js
@@ -50,3 +50,13 @@ function hide(){
   let second = document.getElementById("hello");
   second.style.display= "none";
 }
+
+
+let n = 5;
+let fact = 1;
+
+for (let i = 2; i <= n; i++) {
+  fact *= i;
+}
+
+console.log(fact);
